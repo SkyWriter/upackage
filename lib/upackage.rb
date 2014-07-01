3a2c4468def19732f1bc8854e1ca0287a6cbd5fc
Ivan Kasatenko <sky.31338@gmail.com>
2014-07-01 21:29:23 +0400

    Slice

--- lib/upackage.rb
+++ lib/upackage.rb
@@ -1,5 +1,28 @@
 require "upackage/version"
+require "singleton"
 
 module Upackage
-  # Your code goes here...
+
+  class Creator
+    def initialize(system)
+      @system = system
+    end
+
+    def create
+      system.perform('git clone git@red:sample-project.git')
+      system.perform('dpkg-buildpackage -us -us -b')
+    end
+
+    attr_reader :system
+  end
+
+  class SystemGateway
+    include Singleton
+
+    def perform(command)
+      puts "Performing: #{command}"
+      system(command)
+    end
+  end
+
 end
--- spec/cli/sample_spec.rb
+++ spec/cli/sample_spec.rb
@@ -1,26 +1,3 @@
-require 'singleton'
-
-class Creator
-  def initialize(system)
-    @system = system
-  end
-
-  def create
-    system.perform('git clone git@red:sample-project.git')
-    system.perform('dpkg-buildpackage -us -us -b')
-  end
-
-  attr_reader :system
-end
-
-class SystemGateway
-  include Singleton
-
-  def perform(command)
-    system(command)
-  end
-end
-
 describe Upackage do
 
   it "should build package" do
