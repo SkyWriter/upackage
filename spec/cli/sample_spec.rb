862ce73788ede98cb21c6bc1930e50136027d758
Ivan Kasatenko <sky.31338@gmail.com>
2014-07-01 21:25:13 +0400

    Slice

--- spec/cli/sample_spec.rb
+++ spec/cli/sample_spec.rb
@@ -1,10 +1,38 @@
+require 'singleton'
+
+class Creator
+  def initialize(system)
+    @system = system
+  end
+
+  def create
+    system.perform('git clone git@red:sample-project.git')
+    system.perform('dpkg-buildpackage -us -us -b')
+  end
+
+  attr_reader :system
+end
+
+class SystemGateway
+  include Singleton
+
+  def perform(command)
+    system(command)
+  end
+end
+
 describe Upackage do
 
   it "should build package" do
-    expect(commands).to be == [
+    [
       'git clone git@red:sample-project.git',
-      'dpkg-buildpackage -b -us'
-    ]
+      'dpkg-buildpackage -us -us -b'
+    ].each do |command|
+      expect(SystemGateway.instance).to receive(:perform).with(command)
+    end
+
+    creator = Creator.new(SystemGateway.instance)
+    creator.create
   end
 
 end
