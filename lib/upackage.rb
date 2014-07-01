e59cfb1fa310791da4150ec19cf2e7ca72ec546a
Ivan Kasatenko <sky.31338@gmail.com>
2014-07-01 22:11:58 +0400

    Slice

--- lib/upackage.rb
+++ lib/upackage.rb
@@ -4,19 +4,23 @@ require "singleton"
 module Upackage
 
   class Creator
-    def initialize(system)
+    def initialize(system, fs)
       @system = system
+      @fs = fs
     end
 
     def create
       system.perform('mkdir -p debian/')
-      system.perform("cp -fr #{templates_path}/* debian/")
       system.perform('dpkg-buildpackage -us -us -b')
     end
 
     attr_reader :system
 
     private
+
+    def fill_templates
+      %w(changelog compat control dirs docs files install postinst postrm prerm rules)
+    end
 
     def templates_path
       File.expand_path('../../templates', __FILE__)
@@ -29,6 +33,17 @@ module Upackage
     def perform(command)
       puts "Performing: #{command}"
       system(command)
+    end
+  end
+
+  class FSGateway
+    include Singleton
+
+    def put_file(filename, contents)
+      File.new(filename, "w") do |file|
+        file.write(contents)
+        file.close
+      end
     end
   end
 
--- spec/cli/sample_spec.rb
+++ spec/cli/sample_spec.rb
@@ -3,13 +3,17 @@ describe Upackage do
   it "should build package" do
     [
       'mkdir -p debian/',
-      "cp -fr #{File.expand_path('../../../templates', __FILE__)}/* debian/",
       'dpkg-buildpackage -us -us -b'
     ].each do |command|
       expect(SystemGateway.instance).to receive(:perform).with(command)
     end
 
-    creator = Creator.new(SystemGateway.instance)
+    %w(changelog compat control dirs docs files postinst postrm prerm rules).each do |filename|
+      contents = IO.read(File.expand_path("../../fixtures/#{filename}", __FILE__))
+      expect(FSGateway.instance).to receive(:put_file).with(filename, contents)
+    end
+
+    creator = Creator.new(SystemGateway.instance, FSGateway.instance)
     creator.create
   end
 
