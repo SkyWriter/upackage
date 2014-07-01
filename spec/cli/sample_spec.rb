4feea3b147bf5d8fdc78be7476580c0060a3d29a
Ivan Kasatenko <sky.31338@gmail.com>
2014-07-01 21:16:59 +0400

    Slice

--- spec/cli/sample_spec.rb
+++ spec/cli/sample_spec.rb
@@ -1,7 +1,10 @@
 describe Upackage do
 
   it "should build package" do
-    
+    expect(commands).to be == [
+      'git clone git@red:sample-project.git',
+      'dpkg-buildpackage -b -us'
+    ]
   end
 
 end
